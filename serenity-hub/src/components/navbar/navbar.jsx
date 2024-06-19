8d84c2aec36847a8b0c6aab1c0bb41554b244b86
Stacy-Muhia <128844293+Stacy-Muhia@users.noreply.github.com>
2024-06-19 16:01:34 +0300

    Working on the navbar. Adding logo image and background images

---
+++ serenity-hub/src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <><div className="wrapper"></div>
+        <div className="lwrapper"></div>
+            <div className="rwrapper"></div></>
+  )
+}
+
+export default navbar
